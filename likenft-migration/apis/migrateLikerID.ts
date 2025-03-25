a0ce69660f13ef4e4edc17920debba117af97fb3
Ken Chan <chankiyu22@gmail.com>
2025-03-25 17:53:16 +0800

    likenft-migration: Update migrate liker id api request schema

--- likenft-migration/apis/migrateLikerID.ts
+++ likenft-migration/apis/migrateLikerID.ts
@@ -3,12 +3,14 @@ import { z } from 'zod';
 import { makeAPI } from './makeAPI';
 
 export const RequestSchema = z.object({
+  cosmos_address: z.string(),
   cosmos_pub_key: z.string(),
   like_id: z.string().nullable(),
   eth_address: z.string(),
   cosmos_signature: z.string(),
+  cosmos_signing_message: z.string(),
   eth_signature: z.string(),
-  signing_message: z.string(),
+  eth_signing_message: z.string(),
 });
 
 export const ResponseSchema = z.object({
