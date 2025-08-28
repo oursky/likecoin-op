93473145b7b8fc49380491916b03bd2b6de6e761
Ken Chan <chankiyu22@gmail.com>
2025-08-28 21:59:08 +0800

    likenft: Add Create2BeaconProxy contract
    refs likecoin/likecoin-op#168

---
+++ likenft/contracts/utils/create2/Create2BeaconProxy.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {Create2able} from "./Create2able.sol";
+import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+
+contract Create2BeaconProxy is Create2able {
+    function _create2BeaconProxy(
+        address beacon,
+        bytes memory initData,
+        uint salt
+    ) public returns (address) {
+        bytes memory bytecode = type(BeaconProxy).creationCode;
+        bytes memory arguments = abi.encode(beacon, initData);
+        return create2(_getBytecode(bytecode, arguments), salt);
+    }
+}
